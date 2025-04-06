56bf788db4b047283182f6557a34eec9bcddd34a
Nexter18 <nexterhn@gmail.com>
2025-04-06 12:06:24 -0600

    Fixed errors with terraform and fixed destroy workflow to be triggered manually

--- terraform/aks/outputs.tf
+++ terraform/aks/outputs.tf
@@ -1,9 +1,13 @@
 output "id" {
-  value = data.azurerm_resource_group.example.id
+  value = data.azurerm_resource_group.n5_test_rg.id
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
 
   sensitive = true
+}
+
+output "acr_login_server" {
+  value = data.azurerm_container_registry.acr.login_server
 }
